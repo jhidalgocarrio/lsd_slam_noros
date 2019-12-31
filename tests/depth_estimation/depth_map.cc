eb1f0a58b059c9252be35dda330f4ac595f106c7
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-31 07:19:02 +0000

    Add another test case for intensity grad computation

--- tests/depth_estimation/depth_map.cc
+++ tests/depth_estimation/depth_map.cc
@@ -7,7 +7,15 @@
 using namespace lsd_slam;
 
 TEST_CASE("gradient can be computed", "[gradient]") {
-    std::vector<float> intensities{0, 5, 10};
-    REQUIRE(calc_grad_along_line(intensities, 1.0) == 50.0);
-    REQUIRE(calc_grad_along_line(intensities, 5.0) == 2.0);
+    SECTION("intensity pattern 1") {
+        std::vector<float> intensities{0, 5, 10};
+        REQUIRE(calc_grad_along_line(intensities, 1.0) == 50.0);  // 50 / (1 * 1)
+        REQUIRE(calc_grad_along_line(intensities, 5.0) == 2.0);   // 50 / (5 * 5)
+    }
+
+    SECTION("intensity pattern 2") {
+        std::vector<float> intensities{0, 1, 4};
+        REQUIRE(calc_grad_along_line(intensities, 1.0) == 10.0);  // 10 / (1 * 1)
+        REQUIRE(calc_grad_along_line(intensities, 2.0) == 2.5);  // 10 / (2 * 2)
+    }
 }
